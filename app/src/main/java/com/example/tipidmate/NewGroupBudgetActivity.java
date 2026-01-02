5e4195498ab0aa4480ba2b0d5b529773cb9f8b65
nitoralseanken <nitoralseanken@gmail.com>
2026-01-02 11:49:32 +0800

    commi

---
+++ app/src/main/java/com/example/tipidmate/NewGroupBudgetActivity.java
@@ -0,0 +1,13 @@
+package com.example.tipidmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewGroupBudgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group_budget);
+    }
+}
