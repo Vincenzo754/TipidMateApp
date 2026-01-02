35a5a76587cf03976da09a4a899470248bf7a57c
nitoralseanken <nitoralseanken@gmail.com>
2026-01-02 18:07:21 +0800

    commit

--- app/src/main/java/com/example/tipidmate/MainActivity.java
+++ app/src/main/java/com/example/tipidmate/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
