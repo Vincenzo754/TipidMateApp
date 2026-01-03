8c841e8241230e3d68f0a61bb1c25348c50a126c
nitoralseanken <nitoralseanken@gmail.com>
2026-01-03 15:38:28 +0800

    Update HomeFragment.java

--- app/src/main/java/com/example/tipidmate/HomeFragment.java
+++ app/src/main/java/com/example/tipidmate/HomeFragment.java
@@ -14,7 +14,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // This was the cause of the crash. It's now inflating the correct layout.
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // Corrected to inflate the correct layout file
+        return inflater.inflate(R.layout.home_screen, container, false);
     }
 }
